fc76ed74e7405d9b51255fdb883b859a712c14ed
PitPat1596 <jimmypitte1596@gmail com>
2022-11-10 14:11:15 -0800

    Task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,39 +13,20 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *ptr1;
-	unsigned int x;
-
-	if (!ptr)
-	{
-		ptr1 = malloc(new_size);
-		if (!ptr1)
-			return (NULL);
-		return (ptr1);
-	}
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
 	if (new_size == old_size)
 		return (ptr);
 
-	ptr1 = malloc(new_size);
-
-	if (!ptr1)
-		return (NULL);
-
-	if (new_size < old_size)
-		for (; x < new_size; x++)
-			((char *)ptr1)[x] = ((char *)ptr)[x];
-	else
-		for (; x < old_size; x++)
-			((char *)ptr1)[x] = ((char *)ptr)[x];
-
 	free(ptr);
+	ptr = malloc(new_size);
 
-	return (ptr1);
+	return (ptr);
 }
