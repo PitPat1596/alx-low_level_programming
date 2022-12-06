118b8988b46b60b35701e15e074b0eb6dddb05a2
PitPat1596 <jimmypitte1596@gmail.com>
2022-12-05 21:15:45 -0800

    All task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_textfile(char *filename, char *filecopy, int letters);
-int create_copyfile(const char *filename, char *text_content);
-int append_text_to_copyfile(const char *filename, char *text_content);
 
 #endif
